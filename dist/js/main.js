23bca8e7ec9888fd28e52f888f5bff55cc74f75f
Hugo Machado <hugomachado4@hotmail.com>
2019-08-02 14:22:12 +0100

    Finished drum kit

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,14 +2,18 @@ function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
   }
+  
   function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key-up="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key-up="${e.keyCode}"]`);
     if (!audio) return;
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
   }
-  const keys = Array.from(document.querySelectorAll('.key'));
+
+  const keys = document.querySelectorAll('.key');
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
   window.addEventListener('keydown', playSound);
+
+  
