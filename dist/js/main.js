c6a6d7a6090a52fc2e28259c37f3630901e9e44b
Hugo Machado <hugomachado4@hotmail.com>
2019-08-03 11:24:03 +0100

    Added numpad option

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,19 +1,36 @@
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
-  
-  function playSound(e) {
-    const audio = document.querySelector(`audio[data-key-up="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key-up="${e.keyCode}"]`);
-    if (!audio) return;
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+  let key = '';
+  let audio = '';
+  if(e.keyCode >= 49 && e.keyCode <= 57){
+    audio = document.querySelector(`audio[data-key-up="${e.keyCode}"]`);
+    key = document.querySelector(`div[data-key-up="${e.keyCode}"]`);
+  }else if(e.keyCode >= 97 && e.keyCode <= 105){
+    audio = document.querySelector(`audio[data-key-numpad="${e.keyCode}"]`);
+    key = document.querySelector(`div[data-key-numpad="${e.keyCode}"]`);
+  }else{
+    return;
   }
 
-  const keys = document.querySelectorAll('.key');
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
 
-  
+// function playSoundNumPad(e) {
+//   const audio = document.querySelector(`audio[data-key-numpad="${e.keyCode}"]`);
+//   const key = document.querySelector(`div[data-key-numpad="${e.keyCode}"]`);
+//   if (!audio) return;
+//   key.classList.add('playing');
+//   audio.currentTime = 0;
+//   audio.play();
+// }
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
